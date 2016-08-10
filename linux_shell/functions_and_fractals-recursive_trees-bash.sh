6911dcc7d6479ea4f2e29ab8cb28bc3867accff4
adamatgenia <adam.qwan@roche.com>
2016-08-09 19:08:28 -0700

    added WIP functions_and_fractals-recursive_trees-bash.sh

---
+++ linux_shell/functions_and_fractals-recursive_trees-bash.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+read N
+
+#z=$((x / y))
+
+tree_space=0
+
+for((i=$N;i--;))
+do
+	tree_space=$((tree_space+(32/(2**(N-1)))))
+done
+
+top_space=$((63-$tree_space))
+
+# print empty top space
+for((i=$top_space;i--;))
+do
+    for columns in {1..100}
+    do
+        printf "_"
+    done
+    printf "\n"
+done
+
+# print tree 
+for((i=$N;i--;))
+do
+    branches=$((2**N))
+    inside_padding=$((32/branches))
+    tot_inside_padding=$((inside_spacing*(branches-1)))
+    left_padding=$(((100-branches-inside_padding)/2))
+    right_padding=$((left_padding+branches+tot_inside_padding))
+    rows=$((16/(2**(N-1))))
+    echo $branches
+    echo $inside_padding
+    echo $tot_inside_padding
+    echo $left_padding
+    echo $right_padding
+     
+     
+     
+     
+    #print slants
+    for((i=$rows;i--;))
+    do
+        for column in {1..100};
+        do
+            if [ $((column)) -lt $((left_padding)) ]; then 
+                print "_"; 
+            elif [ $column -gt $right_padding ]; then
+                print "_";
+            elif [ $(((column-left_padding)%(padding+1))) -eq 0 ]; then
+                print "1";
+            else 
+                print "_" 
+            fi       
+        done
+        left_padding+=1
+        right_padding+=-1
+        inside_padding=-2
+        printf "\n"    
+    done
+
+    branches=$((2**N))
+    inside_padding=$((32/branches))
+    total_inside_padding=$((inside_spacing*(branches-1)))
+    left_padding=$(((100-branches-inside_padding)/2))
+    right_padding=
+    rows=$((16/(2**(N-1))))
+
+    #print straights
+    #for((i=$rows;i--;))
+    #do
+    #    for columns in {1..100}
+    #    do
+    #        printf "i"
+    #    done
+    #done
+    #printf "\n"
+    
+done
