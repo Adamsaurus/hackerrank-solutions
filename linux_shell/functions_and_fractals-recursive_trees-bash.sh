d6857fd4b0568e36248efc3bd530b4b8a541dbec
adamatgenia <adam.qwan@roche.com>
2016-08-10 19:41:20 -0700

    update functions_and_fractal solution

--- linux_shell/functions_and_fractals-recursive_trees-bash.sh
+++ linux_shell/functions_and_fractals-recursive_trees-bash.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 read N
-
 #z=$((x / y))
 
 tree_space=0
 
-for((i=$N;i--;));
+for k in `seq $((N))`;
 do
-	tree_space=$((tree_space+(32/(2**(N-1)))))
+    #printf $k
+	tree_space=$((tree_space+(32/(2**(k-1)))))
 done
 
-top_space=$((63-$tree_space))
+top_space=$((63-tree_space))
 
 # print empty top space
-for((i=$top_space;i--;));
+for((l=$top_space;l--;));
 do
     for columns in {1..100};
     do
@@ -22,33 +22,44 @@ do
     printf "\n"
 done
 
-echo $N
+
 
 # print tree 
-for((i=$N;i--;));
+#for ((i=10;i>0;i-=2)) ; do echo -n "$i," ; done
+#for i in `seq $((N))`;
+for((i=$((N));i>0;i-=1))
 do
-    branches=$((2**N))
-    inside_padding=$((32/(branches-1)))
+    #echo $i
+    branches=$((2**i))
+    #if [ $((branches)) -le 2 ]; then
+    #    inside_padding=32
+    #    tot_inside_padding=32
+    #else
+    #    inside_padding=$((2**(6-i)))
+    #    tot_inside_padding=$((inside_padding*(branches-1)))
+    #fi
+    inside_padding=$((2**(6-i)))
     tot_inside_padding=$((inside_padding*(branches-1)))
-    left_padding=$(((102-branches-inside_padding)/2))
+    left_padding=$(((102-branches-tot_inside_padding)/2))
     right_padding=$((left_padding+branches+tot_inside_padding))
-    rows=$((16/(2**(N-1))))
-    echo $branches
-    echo $inside_padding
-    echo $tot_inside_padding
-    echo $left_padding
-    echo $right_padding
+    rows=$((16/(2**(i-1))))
+    #echo $branches
+    #echo $inside_padding
+    #echo $tot_inside_padding
+    #echo $left_padding
+    #echo $right_padding
      
-
     #print slants
-    for((i=$rows;i--;))
+    for((j=$rows;j--;))
     do
         for column in {1..100};
         do
             if [ $((column)) -lt $((left_padding)) ]; then 
                 printf "_"; 
-            elif [ $column -gt $right_padding ]; then
+            elif [ $column -ge $right_padding ]; then
                 printf "_";
+            elif [ $((column-left_padding)) -eq 0 ]; then
+                printf "1";
             elif [ $(((column-left_padding)%inside_padding)) -eq 0 ]; then
                 printf "1";
             else 
@@ -61,21 +72,36 @@ do
         printf "\n"    
     done
 
-    branches=$((2**N))
-    inside_padding=$((32/branches))
-    total_inside_padding=$((inside_spacing*(branches-1)))
-    left_padding=$(((100-branches-inside_padding)/2))
-    right_padding=
-    rows=$((16/(2**(N-1))))
+    branches=$((2**(i-1)))
+    inside_padding=$((32-(2**(6-branches))))
+    tot_inside_padding=$((inside_spacing*(branches-1)))
+    left_padding=$(((101-branches-tot_inside_padding)/2))
+    right_padding=$((left_padding+branches+tot_inside_padding))
+    rows=$((16/(2**(i-1))))
+    #echo $branches
+    #echo $inside_padding
+    #echo $tot_inside_padding
+    #echo $left_padding
+    #echo $right_padding
+    #echo $rows
 
     #print straights
-    #for((i=$rows;i--;))
-    #do
-    #    for columns in {1..100}
-    #    do
-    #        printf "i"
-    #    done
-    #done
-    #printf "\n"
-    
+    for((m=$rows;m--;))
+    do
+        for column in {1..100};
+        do
+            if [ $((column)) -lt $((left_padding)) ]; then 
+                printf "_"; 
+            elif [ $column -ge $right_padding ]; then
+                printf "_";
+            elif [ $((column-left_padding)) -eq 0 ]; then
+                printf "1";
+            elif [ $(((column-left_padding)%inside_padding)) -eq 0 ]; then
+                printf "1";    
+            else 
+                printf "_" 
+            fi       
+        done
+        printf "\n"    
+    done
 done
